07a8d7678e717ffb537d5f1c7d55088c71afbea5
concooking <kovardashov@gmail.com>
2024-10-21 22:29:31 +0300

    Исправил ошибки

--- src/Main.java
+++ src/Main.java
@@ -5,35 +5,32 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         // Задача 1
-        int age = 19;
+        int age = 17;
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + " то он совершеннолетний");
-        }
-        int ageOfMajority = 17;
-        if (ageOfMajority <= 18) {
-            System.out.println("Если возраст человека равен " + ageOfMajority + " то он не достиг совершенолетия, нужно подождать");
+        } else {
+            System.out.println("Если возрваст человека равен " + age + " то возраст совершеннолетия ещё не настал нужно подождать");
         }
         // Задача 2
-        int temp = 7;
+        int temp = 4;
         if (temp < 5) {
             System.out.println("На улице " + temp + " градусов, нужно надеть шапку");
-        }
-        if (temp > 5) {
-            System.out.println("На улице " + temp + " градусов, можно идти без шапки");
+        } else {
+            System.out.println("На улице " + temp + " градусов можно идти без шапки");
         }
         // Задача 3
-        int speed = 50;
+        int speed = 70;
         if (speed > 60) {
             System.out.println("Если скорость " + speed + " то придется заплатить штраф");
         } else {
             System.out.println("Если скорость " + speed + " то можно ездить спокойно");
         }
         // Задача 4
-        int ageMan = 10;
-        if (ageMan > 2 && ageMan < 6) {
+        int ageMan = 4;
+        if (ageMan >= 2 && ageMan <=6) {
             System.out.println("Если возраст человека равен " + ageMan + " то ему нужно ходить в десткий сад");
         }
-        if (ageMan > 7 && ageMan < 17) {
+        if (ageMan >= 7 && ageMan <= 17) {
             System.out.println("Если возраст человека равен " + ageMan + " то ему нужно ходить в школу");
         }
         if (ageMan >= 18 && ageMan <= 24) {
@@ -47,34 +44,34 @@ public class Main {
         if (ageChildren < 5) {
             System.out.println("Если возраст ребенка равен " + ageChildren + " то ему нельзя кататься на аттракционе");
         }
-        if (ageChildren > 5 && ageChildren < 14) {
+        if (ageChildren >= 5 && ageChildren <= 14) {
             System.out.println("Если возраст ребенка равен " + ageChildren + " то ему можно кататься на аттракционе в сопровождении взрослого");
         }
         if (ageChildren > 14) {
             System.out.println("Если возраст ребенка равен " + ageChildren + " то ему можно кататься на аттракционе без сопровождения взрослого");
         }
         // Задача 6
-        int places = 50;
+        int places = 500;
         if (places <= 120) {
             if (places <= 60) {
                 System.out.println("В вагоне есть сидячие места");
             }
             if (places > 60) {
                 System.out.println("В вагоне есть стоячие места");
+            } else {
+                System.out.println("В вагоне мест нет");
             }
-        } else {
-            System.out.println("В вагоне мест нет");
-        }
-        // Задача 7
-        int one = 10;
-        int two = 500;
-        int three = 300;
-        if (one > two && one > three) {
-            System.out.println("Наибольшее число " + one);
-        } else if (two > one && two > three) {
-            System.out.println("Наибольшее число " + two);
-        } else {
-        System.out.println("Наибольшее число " + three);
+            // Задача 7
+            int one = 10;
+            int two = 500;
+            int three = 300;
+            if (one > two && one > three) {
+                System.out.println("Наибольшее число " + one);
+            } else if (two > one && two > three) {
+                System.out.println("Наибольшее число " + two);
+            } else {
+                System.out.println("Наибольшее число " + three);
+            }
         }
     }
 }
